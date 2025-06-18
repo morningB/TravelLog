2fcb801a344c2a8e85b6466f93c3ef3537431ce2
Joowon_Yun <yjw001205@gmail.com>
2025-06-18 17:58:59 +0900

    feat: 물건 다 되찾을 시 이벤트 추가

--- Assets/Scripts/GetItemManager.cs
+++ Assets/Scripts/GetItemManager.cs
@@ -6,25 +6,35 @@ public class GetItemManager : MonoBehaviour
 {
     private RaycastHit hitInfo;
     public int getItemCount;
-    public Text itemText;
+    public int maxItemCount = 5;
+    public Text itemText; 
+    public Text completeText; // 완료시 텍스트 띄우기(다른거로 대체할 예정)
 
+    void Start()
+    {
+        completeText.enabled = false;
+    }
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (getItemCount == maxItemCount)
         {
-            Vector3 mousePos = Input.mousePosition;
-            Ray screenRay = Camera.main.ScreenPointToRay(mousePos);
-
-            if (Physics.Raycast(screenRay.origin, screenRay.direction * 1000f, out hitInfo))
+            completeText.enabled = true;
+        }
+        if (Input.GetMouseButtonDown(0))
             {
-                if (hitInfo.collider.CompareTag("Item"))
-                {
-                    hitInfo.collider.gameObject.SetActive(false);
-                    getItemCount++;
+                Vector3 mousePos = Input.mousePosition;
+                Ray screenRay = Camera.main.ScreenPointToRay(mousePos);
 
-                    itemText.text = "획득 아이템 수 : " + getItemCount + "/ 5";
+                if (Physics.Raycast(screenRay.origin, screenRay.direction * 1000f, out hitInfo))
+                {
+                    if (hitInfo.collider.CompareTag("Item"))
+                    {
+                        hitInfo.collider.gameObject.SetActive(false);
+                        getItemCount++;
+
+                        itemText.text = "획득 아이템 수 : " + getItemCount + "/ " + maxItemCount;
+                    }
                 }
             }
-        }
     }
 }
